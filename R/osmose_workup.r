143c97ceb628fb9b3948f63933b901d4b7418511
Maia Kapur <39382798+mkapur-noaa@users.noreply.github.com>
2024-01-30 09:31:40 -0800

    build multinomial sampler to workup age comps

--- R/osmose_workup.r
+++ R/osmose_workup.r
@@ -32,7 +32,7 @@ spp <- ncvar_get(ncin,"species") ## index spp of interest
 lon <- ncvar_get(ncin,"x")[spp %in% spp_of_interest]
 lat <- ncvar_get(ncin,"y")[spp %in% spp_of_interest]
 age <- ncvar_get(ncin,"age")[spp %in% spp_of_interest]
-abundance <- ncvar_get(ncin,"abundance")[spp %in% spp_of_interest]
+
 
 tmp <- data.frame(species = spp[spp %in% spp_of_interest],
 lon, lat, age, abundance)
@@ -41,10 +41,7 @@ return(tmp)
 get_dat(ncdf_file = ncin) %>%
 group_by(species) %>% summarise(median(abundance), max(abundance))
 
-nc_path <- here::here('data','ns_spatial_abundancebyAge_Simu0_withF',
-'ns_spatial_abundancebyAge-AtlanticCod_Simu0.nc')
-ncin <- nc_open(nc_path)
-get_dat(ncdf_file = ncin) %>% head()
+
 
 step2399 <- get_dat(ncdf_file = ncin) %>%
 filter(abundance< 250000) %>% ## there are some weirdly high values offmap?
@@ -223,9 +220,120 @@ Rmisc::multiplot(maps, mre, rawobs, cols = 3)
 dev.off()
 
 
-## age comps
-nc_path <- here('data','ns_spatialized_Simu0.nc')
+## age comps ----
+nc_path <- here::here('data','ns_spatial_abundancebyAge_Simu0_withF',
+'ns_spatial_abundancebyAge-Whiting_Simu0.nc')
 ncin <- nc_open(nc_path)
-print(ncin)
-names(ncin$var)
-dim(ncin)
+
+abundance0 <- ncvar_get(ncin,"abundance")
+abundance <- reshape2::melt(abundance0) ## this is age (26) lat (25) lon (52) time (24 currently)
+names(abundance) <- c('long','lat','age','timestep','value')
+head(abundance)
+
+
+# Define a function to perform the sampling
+sample_individuals <- function(df, timestep, long, lat) {
+  # Exclude NAs
+  df <- df[!is.na(df$value), ]
+
+# If there are no positive values, return a data frame with zero counts for all age groups
+  if (all(df$value <= 0)) {
+    return(data.frame(timestep = timestep, long = long, lat = lat, 
+    age = 1:max(abundance$age), count = integer(max(abundance$age))))
+  }
+  # Calculate the proportions of each age group
+  props <- df$value / sum(df$value)
+  
+  # Sample 500 individuals using a multinomial distribution
+  sampled <- rmultinom(1, 500, props)
+
+   # Create a data frame with zero counts for all age groups
+  result <- data.frame(timestep = timestep, long = long, lat = lat, 
+  age = 1:max(abundance$age), count = integer(max(abundance$age)))
+  
+  # Fill in the sampled counts for the age groups that were present in the input data
+  result[result$age %in% df$age, "count"] <- sampled
+  
+  # Return the number of individuals in each age group
+  return(result)
+    #return(data.frame(timestep = timestep, long = long, lat = lat, age = df$age, count = result))
+}
+
+# Identify unique combinations of long and lat
+spatial_cells <- unique(abundance[, c("long", "lat")])
+spatial_cells <- spatial_cells[!apply(spatial_cells, 1, function(cell) {
+  all(is.na(abundance[abundance$long == cell["long"] & abundance$lat == cell["lat"], "value"]))
+}), ]
+# Initialize an empty list to store the results
+results <- list()
+
+# For each timestep
+for (timestep in unique(abundance$timestep)) {
+  # Filter the data for the current timestep
+  timestep_data <- abundance[abundance$timestep == timestep, ]
+  
+  # Randomly select 50% of the spatial cells
+  selected_cells <- spatial_cells[sample(nrow(spatial_cells), nrow(spatial_cells) / 2), ]
+  
+  # For each selected cell
+  for (i in 1:nrow(selected_cells)) {
+    # Filter the data for the current cell
+    cell_data <- timestep_data[timestep_data$long == selected_cells$long[i] & 
+                               timestep_data$lat == selected_cells$lat[i], ]
+    
+    # Perform the sampling and store the results
+  results[[paste(timestep, selected_cells$long[i], selected_cells$lat[i], sep = "_")]] <- 
+      sample_individuals(cell_data, timestep, long = selected_cells$long[i], lat = selected_cells$lat[i])
+  }
+}
+
+# Combine the results into a single data frame
+results_df <- do.call(rbind, results)
+
+# Aggregate the data by timestep and age, summing the count
+results_df <- results_df %>%
+  group_by(timestep, age) %>%
+  summarise(count = sum(count))
+
+# Calculate the frequency for each age group within each unique timestep
+results_df <- results_df %>%
+  group_by(timestep) %>%
+  mutate(frequency = count / sum(count))
+
+# Create a new column for the cell identifier
+#results_df$cell <- rownames(results_df)
+#results_df$cell <- interaction(results_df$lat, results_df$long, sep = "_")
+
+# Convert the timestep and cell columns to factors
+##results_df$timestep <- as.factor(results_df$timestep)
+#results_df$cell <- as.factor(results_df$cell)
+
+#results_df %>% group_by(timestep, cell) %>% summarise(n = n())
+
+# Create the plot
+ggplot(results_df, aes(x = age, y = frequency)) +
+  geom_line() +
+  facet_wrap(~ timestep) +
+  labs(x = "Age", y = "Frequency") +
+  theme_minimal()+  theme(legend.position='none')
+
+comps <- abundance %>%
+filter(!is.na(value)) %>%  
+    tidytable::mutate(tot = sum(value), 
+    .by = timestep) %>% 
+    tidytable::summarise(numbers = mean(tot),
+    age_tot = sum(value),
+    .by = c(timestep, age)) %>% 
+    tidytable::mutate(prop = age_tot / numbers) 
+
+
+
+
+comps %>% filter( age == 4)
+
+ggplot(subset(comps, age < 10), aes(x = age, y = prop)) +
+geom_point()+geom_line() +
+facet_wrap(~timestep)
+
+
+
