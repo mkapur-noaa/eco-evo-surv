cd6120b32b30e0061aa588a3720c29262a7604cf
Maia Kapur <39382798+mkapur-noaa@users.noreply.github.com>
2024-04-11 15:24:30 -0700

    rough in parallel of scenarios

--- R/master.r
+++ R/master.r
@@ -2,21 +2,30 @@
 ## execute the full study in parallel
 
 rm(list = ls()) ## clear workspace
-lapply(list.files(here::here('R','functions'), full.names = TRUE), FUN=source) ## load all functions and presets
+invisible(lapply(list.files(here::here('R','functions'), full.names = TRUE), FUN=source)) ## load all functions and presets
 
 cores <- detectCores() - 2
 cl <- makeCluster(cores)
 registerDoParallel(cl)
 
-parallelized_sim <- foreach(replic = reps_run, .combine = cbind) %dopar% {
-  #source(here::here("R",'load_files_EM.R'))
-  #expID = z # HCRS 1 = status quo, 2 = eHCR, 3 = mean catch x fleet 2015-2019
-  #spaceID = s  # spatial structure
-  ## 1 = like OM, 
-  ## 2 = 6 subareas, stocks = mgmt, with movement 
-  ## 3 = 3 subareas = stocks = mgmt, with movement
-  ## 4 = 3 subareas = stocks = mgmt, no movement
-  ## 5 = 1 subarea, stock, mgmt (panmixia)
+#1,7,9,13
+
+build_Data(scenario = 2,
+sppIdx = 9, 
+replicate = 1,
+yrs_use = 2010:2044)
+
+foreach(scenario=1:4) %dopar% {
+invisible(lapply(list.files(here::here('R','functions'), full.names = TRUE), FUN=source)) ## load all functions and presets
+
+build_Data(scenario,
+sppIdx = 1,
+ repuse = 1,
+ yrs_use = 2010:2044, 
+ obs_error = 0.2, ## whether or not survey biomass has observation error
+ units = 'numbers',
+  units_scalar = 1e6)
+  #
   #sim <- replicate(1,  {retry_function(function()run_MSE(nprojyrs, neqyrs,spaceID), 1000)})
 }
 # When you're done, clean up the cluster
