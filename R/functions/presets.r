484751f54d89666712f4b1a8b5ec9c0883eee55e
Maia Kapur <39382798+mkapur-noaa@users.noreply.github.com>
2024-04-15 09:47:51 -0700

    cleanup preset formatting

--- R/functions/presets.r
+++ R/functions/presets.r
@@ -2,8 +2,8 @@
 ## establish packages, plotting settings, and directories
 
 ## workup of evosmose outputs
-#0=AtlanticHerring 1=AtlanticMackerel 2=Sandeel 3=EuropeanSprat 4=NorwayPout 
-# 5=EuropeanPlaice 6=CommonSole 7=Saithe 8=AtlanticCod 9=Haddock 10=HorseMackerel 
+#0=AtlanticHerring 1=AtlanticMackerel 2=Sandeel 3=EuropeanSprat 4=NorwayPout
+# 5=EuropeanPlaice 6=CommonSole 7=Saithe 8=AtlanticCod 9=Haddock 10=HorseMackerel
 ##11=Whiting 12=CommonDab 13=GreyGurnard 14=Hake 15=Shrimp
 #https://pjbartlein.github.io/REarthSysSci/netCDF.html#reading-restructuring-and-writing-netcdf-files-in-r
 #install.packages('ncdf4')
@@ -23,16 +23,16 @@ options(dplyr.summarise.inform = FALSE)
 
 ## definitions and labels
 scenLabs <-  c('No Climate Change & No Evolution',
-'No Climate Change & Evolution',
-'Climate Change & No Evolution',
-'Climate Change & Evolution')
+               'No Climate Change & Evolution',
+               'Climate Change & No Evolution',
+               'Climate Change & Evolution')
 names(scenLabs) <- c('noCC_noEvo','noCC_Evo','CC_noEvo','CC_Evo')
 
 
 
 sppLabs <-c("Atlantic Herring" , "Atlantic Mackerel" ,"Sandeel" ,"European Sprat","Norway Pout" ,
-"European Plaice","Common Sole","Saithe","Atlantic Cod","Haddock","Horse Mackerel" ,
-"Whiting","Common Dab","Grey Gurnard" ,"Hake","Shrimp")
+            "European Plaice","Common Sole","Saithe","Atlantic Cod","Haddock","Horse Mackerel" ,
+            "Whiting","Common Dab","Grey Gurnard" ,"Hake","Shrimp")
 #names(sppLabs) <- as.character(0:15)
 names(sppLabs) <- gsub(" ","",sppLabs)
 sppLabs2 <- cbind(Var1 = sppLabs, Var2=names(sppLabs), Var3 = 0:(length(sppLabs)-1))
