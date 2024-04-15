4b38d9df9b3a13515cdbd131ae6170f32232f9e9
Maia Kapur <39382798+mkapur-noaa@users.noreply.github.com>
2024-04-15 09:47:20 -0700

    clean up formatting

--- R/functions/_sampler.r
+++ R/functions/_sampler.r
@@ -1,13 +1,9 @@
 ## sampler functions
 ## standalone functions to sample individuals from a population
 ## consider NA handling (precludes a fixed set of cells for all spp/yea)
-##
 
-
-## established fixed sampling design for each year
+## establish fixed sampling design for each year
 ## ensure that the survey is truly multispecies, sampling at the same location for all species in a given year
-
-
 
 build_survey_array <- function(n_years = length(2010:2099), fractional_coverage = 0.5){
   #Create a data frame with all combinations of lat and long
@@ -30,26 +26,26 @@ sample_ages <- function(df, timestep, long, lat) {
   # Exclude NAs
   df <- df[!is.na(df$value), ]
 
-# If there are no positive values, return a data frame with zero counts for all age groups
+  # If there are no positive values, return a data frame with zero counts for all age groups
   #if (all(df$value <= 0) | nrow(df) == 0){
-  #  return(data.frame(timestep = timestep, long = long, lat = lat, 
+  #  return(data.frame(timestep = timestep, long = long, lat = lat,
   #  age = 1:26, count =0))
   #}
   # Calculate the proportions of each age group
   props <- df$value / sum(df$value)
-  
+
   # Sample 500 individuals using a multinomial distribution
   sampled <- rmultinom(1, 500, props)
 
-   # Create a data frame with zero counts for all age groups
-  result <- data.frame(timestep = timestep, long = long, lat = lat, 
-  age = 1:max(df$age), count = integer(max(df$age)))
-  
+  # Create a data frame with zero counts for all age groups
+  result <- data.frame(timestep = timestep, long = long, lat = lat,
+                       age = 1:max(df$age), count = integer(max(df$age)))
+
   # Fill in the sampled counts for the age groups that were present in the input data
   result[result$age %in% df$age, "count"] <- sampled
-  
+
   # Return the number of individuals in each age group
   return(result)
-    #return(data.frame(timestep = timestep, long = long, lat = lat, age = df$age, count = result))
+  #return(data.frame(timestep = timestep, long = long, lat = lat, age = df$age, count = result))
 }
 
