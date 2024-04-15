0b49366298f64643ebeccd8baf053c85f90a7b09
Maia Kapur <39382798+mkapur-noaa@users.noreply.github.com>
2024-04-15 09:47:37 -0700

    cleanup formats

--- R/functions/build_Data.r
+++ R/functions/build_Data.r
@@ -14,191 +14,191 @@
 ## units_scalar: optional factor to decrease the scale of the index data (applies to comps as well)
 
 
-build_Data<-function(scenario, 
-sppIdx,
- repuse = 1,
- yrs_use = 2010:2099, 
- obs_error = 0.2, ## whether or not survey biomass has observation error
- units = 'numbers',
-  units_scalar = 1e6){
+build_Data<-function(scenario,
+                     sppIdx,
+                     repuse = 1,
+                     yrs_use = 2010:2099,
+                     obs_error = 0.2, ## whether or not survey biomass has observation error
+                     units = 'numbers',
+                     units_scalar = 1e6){
 
-dirtmp <- here::here('data','Ev-OSMOSE outputs',paste0('Ev-osmose_',scenLabs2[scenario,2]),'output')
+  dirtmp <- here::here('data','Ev-OSMOSE outputs',paste0('Ev-osmose_',scenLabs2[scenario,2]),'output')
 
-## strip and format catches (yr x Age)
-yield_files <- list.files(dirtmp, pattern = 'ns_yield*', recursive = T, full.names = TRUE)
+  ## strip and format catches (yr x Age)
+  yield_files <- list.files(dirtmp, pattern = 'ns_yield*', recursive = T, full.names = TRUE)
 
-## sample and build index inputs  (year, index as biom/numbers, cv, vector of ages in numbers/biomass, inputN for comps)
-units_use <- ifelse(units == 'numbers','abundance','biomass') ## how the files are labeled
+  ## sample and build index inputs  (year, index as biom/numbers, cv, vector of ages in numbers/biomass, inputN for comps)
+  units_use <- ifelse(units == 'numbers','abundance','biomass') ## how the files are labeled
 
-age_spatial_path <- list.files(dirtmp, pattern = paste0('spatial_',units_use,'byAge-',sppLabs2[sppIdx,2]), recursive = T,
- full.names = TRUE)[repuse]
-repID <-  as.numeric(stringr::str_extract(age_spatial_path, "(?<=Simu)\\d+(?=\\.nc)")) ## might not match replicate input
+  age_spatial_path <- list.files(dirtmp, pattern = paste0('spatial_',units_use,'byAge-',sppLabs2[sppIdx,2]), recursive = T,
+                                 full.names = TRUE)[repuse]
+  repID <-  as.numeric(stringr::str_extract(age_spatial_path, "(?<=Simu)\\d+(?=\\.nc)")) ## might not match replicate input
 
-abundance0 <- ncvar_get(nc_open(age_spatial_path),"abundance") ## this might need to switch with units_use
-## this is age (26) lat (25) lon (52) timestep (24/year 2010-2099)
-## redefine the timesteps into real year, week
-## and then only take out the July populations
-abundance <- reshape2::melt(abundance0) %>%
-  mutate(year = 2010 + (Var4 - 1) %/% 24,
-    month = ((Var4 - 1) %% 24) %/% 2 + 1) %>%
+  abundance0 <- ncvar_get(nc_open(age_spatial_path),"abundance") ## this might need to switch with units_use
+  ## this is age (26) lat (25) lon (52) timestep (24/year 2010-2099)
+  ## redefine the timesteps into real year, week
+  ## and then only take out the July populations
+  abundance <- reshape2::melt(abundance0) %>%
+    mutate(year = 2010 + (Var4 - 1) %/% 24,
+           month = ((Var4 - 1) %% 24) %/% 2 + 1) %>%
     select(lat=Var1, long=Var2, age=Var3, year, value, month) %>%
     filter(month == 7 & year %in% yrs_use) %>%
     group_by(year, age, lat, long, month) %>%
     summarise(value = mean(value)) %>% ## average over the month
     ungroup() %>%
-    select(-month)  
+    select(-month)
 
 
-total_area <- length(unique(abundance$lat))*length(unique(abundance$long))
-
-
-
-# Initialize an empty list to store the results
-results_age <- results_index <- list()
-
-# For each timestep
-for (timestep in unique(abundance$year)) {
-  if(timestep %%2!=0) next ## only even years
-  cat(timestep,"\n")
-  # Filter the data for the current timestep
-  timestep_data <- abundance[abundance$year == timestep, ]
-
-# pull out the survey stations for this year
-  selected_cells <- survey_array[survey_array$year == timestep, ]
-
-# expand the mean and sd of the abundance for the selected cells
-# this approach has NO survey selectivity
-survey_abund <- semi_join(timestep_data, selected_cells, by = c("lat", "long")) %>%
-filter(!is.na(value)) %>% ## remove NAs
-group_by(lat, long) %>%
-summarise(station_abund = ifelse(is.null(obs_error), 
-sum(value),
-rnorm(1,sum(value),obs_error*mean(value)))) %>% ## sum over all ages
-ungroup() %>%
-summarise(
-abund_mean = mean(station_abund)*total_area, 
-abund_sd = sqrt(var(station_abund, na.rm = T)*total_area^2 / nrow(selected_cells)),
-abund_cv = abund_sd/abund_mean ) %>%
-mutate(year = timestep, replicate = repID, scenario = scenario, species = sppLabs2[sppIdx,2])
-
-results_index[[paste(timestep)]] <- survey_abund
+  total_area <- length(unique(abundance$lat))*length(unique(abundance$long))
 
 
 
-## perform multinomial sampling of ages at each station
-  for (i in 1:nrow(selected_cells)) {
-    # Filter the data for the current cell (all ages present in population)
-    cell_data <- timestep_data[timestep_data$long == selected_cells$long[i] & 
-                               timestep_data$lat == selected_cells$lat[i], ]
+  # Initialize an empty list to store the results
+  results_age <- results_index <- list()
 
-     if(all(is.na(cell_data$value))) next ## skip if no data                    
-    # Perform the age sampling and store the results
-  results_age[[paste(timestep, selected_cells$long[i], selected_cells$lat[i], sep = "_")]] <- 
-      sample_ages(cell_data, timestep, long = selected_cells$long[i], lat = selected_cells$lat[i])
+  # For each timestep
+  for (timestep in unique(abundance$year)) {
+    if(timestep %%2!=0) next ## only even years
+    cat(timestep,"\n")
+    # Filter the data for the current timestep
+    timestep_data <- abundance[abundance$year == timestep, ]
 
-  } ## end stations loop for age comps
-} ## end timesteps loop for survey data
+    # pull out the survey stations for this year
+    selected_cells <- survey_array[survey_array$year == timestep, ]
 
-# Combine the results into a single data frame
-# define maximum age above which all entries are NA
-max_age <- abundance %>%
-  group_by(age) %>%
-  #summarise(mean(value, na.RM = TRUE)) %>% tail(10)
-  summarise(all_zero = all(value == 0)) %>% 
-  filter(!is.na(all_zero)) %>%
-  summarise(max_age = max(age))
+    # expand the mean and sd of the abundance for the selected cells
+    # this approach has NO survey selectivity
+    survey_abund <- semi_join(timestep_data, selected_cells, by = c("lat", "long")) %>%
+      filter(!is.na(value)) %>% ## remove NAs
+      group_by(lat, long) %>%
+      summarise(station_abund = ifelse(is.null(obs_error),
+                                       sum(value),
+                                       rnorm(1,sum(value),obs_error*mean(value)))) %>% ## sum over all ages
+      ungroup() %>%
+      summarise(
+        abund_mean = mean(station_abund)*total_area,
+        abund_sd = sqrt(var(station_abund, na.rm = T)*total_area^2 / nrow(selected_cells)),
+        abund_cv = abund_sd/abund_mean ) %>%
+      mutate(year = timestep, replicate = repID, scenario = scenario, species = sppLabs2[sppIdx,2])
 
-results_df_age_spatial <- do.call(rbind, results_age) %>% filter(age <= as.numeric(max_age))
-results_df_index <- do.call(rbind, results_index)
-
-# Aggregate the agecomp data by timestep and age, summing the count (collapse space)
-results_df_age <- results_df_age_spatial %>%
-  group_by(timestep, age) %>%
-  summarise(count = sum(count)) %>%
-  select(year = timestep, age, count)
-
-
-# rescale, reshape to WHAM format and save
-survey_results <- results_df_index %>%
-mutate(abund_mean= abund_mean/units_scalar)%>%
-select(year, abund_mean, abund_cv) %>%
-merge(., results_df_age %>% mutate(count = count/units_scalar) %>% tidyr::pivot_wider(., names_from = age, values_from = count), by = 'year') %>%
-mutate(inputN = 50)
-
-# save the results
-write.csv(survey_results,  
-here::here('data','wham_inputs',paste0(sppLabs2[sppIdx,2],'-rep',repID,'-',scenLabs2[scenario,2],'-wham_survey.csv')), 
-row.names = FALSE)
-
-
-## summary figures
-
-## maps of true abundance thru time
-map <- abundance %>%
-group_by(year, lat, long) %>%
-summarise(tot_val = sum(value)) %>%
-mutate(abundance_rescale =  rescale(tot_val, to=c(0,1), na.rm = T)) %>% 
-ungroup() %>%
-filter(year %in% seq(2015,2075,length.out = 4))  %>% 
-ggplot(data = ., aes(x = lat, y = long, 
-fill = abundance_rescale))+
-theme_void()+
-geom_raster()+
-scale_fill_viridis_c(na.value = NA)+
-theme(strip.text = element_text(size = 25), 
-strip.text.y = element_blank(),
-legend.position = 'none')+
-facet_wrap(~year, ncol = 2)
-
-
-## survey index data
-true_abund <- abundance %>% 
-group_by(year) %>% 
-summarise(abund_mean=sum(value,na.rm = T)/units_scalar) %>% 
-ungroup()
-
-true_b <- ggplot(true_abund, aes(x = year, y = abund_mean)) +
-geom_line(color = scenLabs2[scenario,'Pal'])+
-scale_x_continuous(breaks = seq(min(yrs_use), max(yrs_use), by = 10))+
-labs(x = 'Year', y = paste0('True Abundance ',
-ifelse(units == 'numbers','(millions)','(tons)'))) 
-
-
-index <- ggplot(survey_results, aes(x = year, y = abund_mean)) +
-geom_point(color = scenLabs2[scenario,'Pal'])+
-#geom_line(data = true_abund, color = 'red')+
-geom_errorbar(aes(ymin = abund_mean - abund_cv*abund_mean, 
-ymax = abund_mean + abund_cv*abund_mean), width = 0, color = scenLabs2[scenario,'Pal']) +
-scale_x_continuous(breaks = seq(min(yrs_use), max(yrs_use), by = 10))+
-labs(x = 'Year', y = paste0('Survey Abundance ',
-ifelse(units == 'numbers','(millions)','(tons)'))) 
-
-## survey age comps
-comps <- results_df_age %>%
-group_by(year) %>%
-mutate(frequency = count / sum(count)) %>%
-ungroup() %>%
-ggplot(., aes(x = age, y = frequency, group = year, color = factor(year))) +
-geom_line()+ 
-scale_x_continuous(breaks = seq(0, max(results_df_age$age), by = 5))+
-labs(x = 'Age', y = 'Frequency', color = 'Year') + 
-scale_color_manual(values =  monochromeR::generate_palette(scenLabs2[scenario,'Pal'], modification = "go_lighter", 
-                 n_colours = length(yrs_use), view_palette = TRUE))+
-theme(legend.position = 'none')
+    results_index[[paste(timestep)]] <- survey_abund
 
 
 
-png(file = here::here('figs',paste0(sppLabs2[sppIdx,2],'-rep',repID,'-',
-scenLabs2[scenario,2],'-abundance-',units,'-',Sys.Date(),'.png')),
-height = 8, width = 12, unit = 'in',res = 520)
-#Rmisc::multiplot(map, index, cols = 2)
-Rmisc::multiplot(map, index, true_b, cols = 3)
+    ## perform multinomial sampling of ages at each station
+    for (i in 1:nrow(selected_cells)) {
+      # Filter the data for the current cell (all ages present in population)
+      cell_data <- timestep_data[timestep_data$long == selected_cells$long[i] &
+                                   timestep_data$lat == selected_cells$lat[i], ]
 
-dev.off()
+      if(all(is.na(cell_data$value))) next ## skip if no data
+      # Perform the age sampling and store the results
+      results_age[[paste(timestep, selected_cells$long[i], selected_cells$lat[i], sep = "_")]] <-
+        sample_ages(cell_data, timestep, long = selected_cells$long[i], lat = selected_cells$lat[i])
+
+    } ## end stations loop for age comps
+  } ## end timesteps loop for survey data
+
+  # Combine the results into a single data frame
+  # define maximum age above which all entries are NA
+  max_age <- abundance %>%
+    group_by(age) %>%
+    #summarise(mean(value, na.RM = TRUE)) %>% tail(10)
+    summarise(all_zero = all(value == 0)) %>%
+    filter(!is.na(all_zero)) %>%
+    summarise(max_age = max(age))
+
+  results_df_age_spatial <- do.call(rbind, results_age) %>% filter(age <= as.numeric(max_age))
+  results_df_index <- do.call(rbind, results_index)
+
+  # Aggregate the agecomp data by timestep and age, summing the count (collapse space)
+  results_df_age <- results_df_age_spatial %>%
+    group_by(timestep, age) %>%
+    summarise(count = sum(count)) %>%
+    select(year = timestep, age, count)
 
 
-cat('Built data and summary figures for',paste0(sppLabs2[sppIdx,2],
-' ',scenLabs2[scenario,2],' replicate ',
-repID))
+  # rescale, reshape to WHAM format and save
+  survey_results <- results_df_index %>%
+    mutate(abund_mean= abund_mean/units_scalar)%>%
+    select(year, abund_mean, abund_cv) %>%
+    merge(., results_df_age %>% mutate(count = count/units_scalar) %>% tidyr::pivot_wider(., names_from = age, values_from = count), by = 'year') %>%
+    mutate(inputN = 50)
+
+  # save the results
+  write.csv(survey_results,
+            here::here('data','wham_inputs',paste0(sppLabs2[sppIdx,2],'-rep',repID,'-',scenLabs2[scenario,2],'-wham_survey.csv')),
+            row.names = FALSE)
+
+
+  ## summary figures
+
+  ## maps of true abundance thru time
+  map <- abundance %>%
+    group_by(year, lat, long) %>%
+    summarise(tot_val = sum(value)) %>%
+    mutate(abundance_rescale =  rescale(tot_val, to=c(0,1), na.rm = T)) %>%
+    ungroup() %>%
+    filter(year %in% seq(2015,2075,length.out = 4))  %>%
+    ggplot(data = ., aes(x = lat, y = long,
+                         fill = abundance_rescale))+
+    theme_void()+
+    geom_raster()+
+    scale_fill_viridis_c(na.value = NA)+
+    theme(strip.text = element_text(size = 25),
+          strip.text.y = element_blank(),
+          legend.position = 'none')+
+    facet_wrap(~year, ncol = 2)
+
+
+  ## survey index data
+  true_abund <- abundance %>%
+    group_by(year) %>%
+    summarise(abund_mean=sum(value,na.rm = T)/units_scalar) %>%
+    ungroup()
+
+  true_b <- ggplot(true_abund, aes(x = year, y = abund_mean)) +
+    geom_line(color = scenLabs2[scenario,'Pal'])+
+    scale_x_continuous(breaks = seq(min(yrs_use), max(yrs_use), by = 10))+
+    labs(x = 'Year', y = paste0('True Abundance ',
+                                ifelse(units == 'numbers','(millions)','(tons)')))
+
+
+  index <- ggplot(survey_results, aes(x = year, y = abund_mean)) +
+    geom_point(color = scenLabs2[scenario,'Pal'])+
+    #geom_line(data = true_abund, color = 'red')+
+    geom_errorbar(aes(ymin = abund_mean - abund_cv*abund_mean,
+                      ymax = abund_mean + abund_cv*abund_mean), width = 0, color = scenLabs2[scenario,'Pal']) +
+    scale_x_continuous(breaks = seq(min(yrs_use), max(yrs_use), by = 10))+
+    labs(x = 'Year', y = paste0('Survey Abundance ',
+                                ifelse(units == 'numbers','(millions)','(tons)')))
+
+  ## survey age comps
+  comps <- results_df_age %>%
+    group_by(year) %>%
+    mutate(frequency = count / sum(count)) %>%
+    ungroup() %>%
+    ggplot(., aes(x = age, y = frequency, group = year, color = factor(year))) +
+    geom_line()+
+    scale_x_continuous(breaks = seq(0, max(results_df_age$age), by = 5))+
+    labs(x = 'Age', y = 'Frequency', color = 'Year') +
+    scale_color_manual(values =  monochromeR::generate_palette(scenLabs2[scenario,'Pal'], modification = "go_lighter",
+                                                               n_colours = length(yrs_use), view_palette = TRUE))+
+    theme(legend.position = 'none')
+
+
+
+  png(file = here::here('figs',paste0(sppLabs2[sppIdx,2],'-rep',repID,'-',
+                                      scenLabs2[scenario,2],'-abundance-',units,'-',Sys.Date(),'.png')),
+      height = 8, width = 12, unit = 'in',res = 520)
+  #Rmisc::multiplot(map, index, cols = 2)
+  Rmisc::multiplot(map, index, true_b, cols = 3)
+
+  dev.off()
+
+
+  cat('Built data and summary figures for',paste0(sppLabs2[sppIdx,2],
+                                                  ' ',scenLabs2[scenario,2],' replicate ',
+                                                  repID))
 
 }
--- R/master.r
+++ R/master.r
@@ -11,25 +11,25 @@ registerDoParallel(cl)
 #1,7,9,13
 
 build_Data(scenario = 2,
-sppIdx = 7, 
-replicate = 1,
-yrs_use = 2010:2044)
+           sppIdx = 7,
+           replicate = 1,
+           yrs_use = 2010:2044)
 
 ## for one species-replicate combo, four scenarios takes about 20 seconds
 foreach(scenario=1:4) %dopar% {
-invisible(lapply(list.files(here::here('R','functions'), full.names = TRUE), FUN=source)) ## load all functions and presets
+  invisible(lapply(list.files(here::here('R','functions'), full.names = TRUE), FUN=source)) ## load all functions and presets
 
-for(species in c(1,7,9,13)) {
-build_Data(scenario,
-sppIdx = species,
- repuse = 1,
- yrs_use = 2010:2044, 
- obs_error = 0.2, ## whether or not survey biomass has observation error
- units = 'numbers',
-  units_scalar = 1e6)
-  #
-  #sim <- replicate(1,  {retry_function(function()run_MSE(nprojyrs, neqyrs,spaceID), 1000)})
-} ## end species loop
+  for(species in c(1,7,9,13)) {
+    build_Data(scenario,
+               sppIdx = species,
+               repuse = 1,
+               yrs_use = 2010:2044,
+               obs_error = 0.2, ## whether or not survey biomass has observation error
+               units = 'numbers',
+               units_scalar = 1e6)
+    #
+    #sim <- replicate(1,  {retry_function(function()run_MSE(nprojyrs, neqyrs,spaceID), 1000)})
+  } ## end species loop
 } ## end scenario loop (parallel)
 
 # When you're done, clean up the cluster
