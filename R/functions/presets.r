dabca697a3725ef84caf6a70a5ed59ee4262c869
Maia Kapur <39382798+mkapur-noaa@users.noreply.github.com>
2024-04-26 12:12:35 -0700

    one-time extraction of length weight pars

--- R/functions/presets.r
+++ R/functions/presets.r
@@ -2,8 +2,8 @@
 ## establish packages, plotting settings, and directories
 
 ## workup of evosmose outputs
-#0=AtlanticHerring 1=AtlanticMackerel 2=Sandeel 3=EuropeanSprat 4=NorwayPout
-# 5=EuropeanPlaice 6=CommonSole 7=Saithe 8=AtlanticCod 9=Haddock 10=HorseMackerel
+#0=AtlanticHerring 1=AtlanticMackerel 2=Sandeel 3=EuropeanSprat 4=NorwayPout 
+# 5=EuropeanPlaice 6=CommonSole 7=Saithe 8=AtlanticCod 9=Haddock 10=HorseMackerel 
 ##11=Whiting 12=CommonDab 13=GreyGurnard 14=Hake 15=Shrimp
 #https://pjbartlein.github.io/REarthSysSci/netCDF.html#reading-restructuring-and-writing-netcdf-files-in-r
 #install.packages('ncdf4')
@@ -15,6 +15,7 @@ library(scales)
 library(foreach)
 library(doParallel)
 library(testthat)
+library(wham) 
 # remotes::install_github("cararthompson/monochromeR"
 library(monochromeR)
 
@@ -23,16 +24,16 @@ options(dplyr.summarise.inform = FALSE)
 
 ## definitions and labels
 scenLabs <-  c('No Climate Change & No Evolution',
-               'No Climate Change & Evolution',
-               'Climate Change & No Evolution',
-               'Climate Change & Evolution')
+'No Climate Change & Evolution',
+'Climate Change & No Evolution',
+'Climate Change & Evolution')
 names(scenLabs) <- c('noCC_noEvo','noCC_Evo','CC_noEvo','CC_Evo')
 
 
 
 sppLabs <-c("Atlantic Herring" , "Atlantic Mackerel" ,"Sandeel" ,"European Sprat","Norway Pout" ,
-            "European Plaice","Common Sole","Saithe","Atlantic Cod","Haddock","Horse Mackerel" ,
-            "Whiting","Common Dab","Grey Gurnard" ,"Hake","Shrimp")
+"European Plaice","Common Sole","Saithe","Atlantic Cod","Haddock","Horse Mackerel" ,
+"Whiting","Common Dab","Grey Gurnard" ,"Hake","Shrimp")
 #names(sppLabs) <- as.character(0:15)
 names(sppLabs) <- gsub(" ","",sppLabs)
 sppLabs2 <- cbind(Var1 = sppLabs, Var2=names(sppLabs), Var3 = 0:(length(sppLabs)-1))
@@ -43,6 +44,15 @@ scenPal <- c('#a00000','#d8a6a6','#9fc8c8','#298c8c') ## four scenario colors: r
 scenLabs2 <- cbind(Var1 = scenLabs, Var2=names(scenLabs), Var3 = 1:4, Pal = rev(scenPal))
 sppPal <- gplots::rich.colors(n = 13)
 
-survey_array <- build_survey_array(fractional_coverage = 0.15)
+survey_array <- build_survey_array(fractional_coverage = 0.3)
 
 
+ # parm_path <- "F:/Ev-osmose/Ev-OSMOSE outputs_15April2024/cc_evo/ns_param-species.csv" ## stored once for all runs, not time-invariant
+  #read.csv(parm_path, header = F) %>%
+ # filter(grepl('length2weight',V1)) %>% 
+#  mutate(sppIdxTEMP = stringr::str_extract(V1, "\\d+$"),
+#   variable = stringr::str_extract(V1, "(?<=length2weight\\.)\\w+(?=\\.)")) %>%
+ # merge(., sppLabs2, by.x = 'sppIdxTEMP', by.y = 'Var3') %>% 
+#  select(species0 =  sppIdxTEMP, species = Var2, variable, value = V2) %>%
+#  tidyr::pivot_wider(names_from = variable, values_from = value) %>%
+#  write.csv(here::here('outputs','wham_runs','length2weight.csv'), row.names = FALSE)
