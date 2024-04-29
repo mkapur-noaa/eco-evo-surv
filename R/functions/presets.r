827ee82a40ed8a11d03b33177c51f901d9eb1b1a
Maia Kapur <39382798+mkapur-noaa@users.noreply.github.com>
2024-04-29 10:23:43 -0700

    formatting and parameter lookup

--- R/functions/presets.r
+++ R/functions/presets.r
@@ -1,10 +1,12 @@
 ## eco-evo-surv settings
 ## establish packages, plotting settings, and directories
 
-## workup of evosmose outputs
-#0=AtlanticHerring 1=AtlanticMackerel 2=Sandeel 3=EuropeanSprat 4=NorwayPout 
-# 5=EuropeanPlaice 6=CommonSole 7=Saithe 8=AtlanticCod 9=Haddock 10=HorseMackerel 
+
+#0=AtlanticHerring 1=AtlanticMackerel 2=Sandeel 3=EuropeanSprat 4=NorwayPout
+# 5=EuropeanPlaice 6=CommonSole 7=Saithe 8=AtlanticCod 9=Haddock 10=HorseMackerel
 ##11=Whiting 12=CommonDab 13=GreyGurnard 14=Hake 15=Shrimp
+
+## packages ----
 #https://pjbartlein.github.io/REarthSysSci/netCDF.html#reading-restructuring-and-writing-netcdf-files-in-r
 #install.packages('ncdf4')
 library(ncdf4)
@@ -15,14 +17,14 @@ library(scales)
 library(foreach)
 library(doParallel)
 library(testthat)
-library(wham) 
+library(wham)
 # remotes::install_github("cararthompson/monochromeR"
 library(monochromeR)
 
 theme_set(ggsidekick::theme_sleek())
 options(dplyr.summarise.inform = FALSE)
 
-## definitions and labels
+## definitions and labels ----
 scenLabs <-  c('No Climate Change & No Evolution',
 'No Climate Change & Evolution',
 'Climate Change & No Evolution',
@@ -39,7 +41,7 @@ names(sppLabs) <- gsub(" ","",sppLabs)
 sppLabs2 <- cbind(Var1 = sppLabs, Var2=names(sppLabs), Var3 = 0:(length(sppLabs)-1))
 
 
-## color palettes
+## color palettes ----
 scenPal <- c('#a00000','#d8a6a6','#9fc8c8','#298c8c') ## four scenario colors: reds have climate change, blues do not
 scenLabs2 <- cbind(Var1 = scenLabs, Var2=names(scenLabs), Var3 = 1:4, Pal = rev(scenPal))
 sppPal <- gplots::rich.colors(n = 13)
@@ -47,12 +49,16 @@ sppPal <- gplots::rich.colors(n = 13)
 survey_array <- build_survey_array(fractional_coverage = 0.3)
 
 
- # parm_path <- "F:/Ev-osmose/Ev-OSMOSE outputs_15April2024/cc_evo/ns_param-species.csv" ## stored once for all runs, not time-invariant
-  #read.csv(parm_path, header = F) %>%
- # filter(grepl('length2weight',V1)) %>% 
+## build and save parameter lookup tables ----
+
+# parm_path <- "F:/Ev-osmose/Ev-OSMOSE outputs_15April2024/cc_evo/ns_param-species.csv" ## stored once for all runs, not time-invariant
+
+#* length-weight allometric pars ----
+# read.csv(parm_path, header = F) %>%
+# filter(grepl('length2weight',V1)) %>%
 #  mutate(sppIdxTEMP = stringr::str_extract(V1, "\\d+$"),
 #   variable = stringr::str_extract(V1, "(?<=length2weight\\.)\\w+(?=\\.)")) %>%
- # merge(., sppLabs2, by.x = 'sppIdxTEMP', by.y = 'Var3') %>% 
+# merge(., sppLabs2, by.x = 'sppIdxTEMP', by.y = 'Var3') %>%
 #  select(species0 =  sppIdxTEMP, species = Var2, variable, value = V2) %>%
 #  tidyr::pivot_wider(names_from = variable, values_from = value) %>%
 #  write.csv(here::here('outputs','wham_runs','length2weight.csv'), row.names = FALSE)
