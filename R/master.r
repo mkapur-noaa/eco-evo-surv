56b03000f7f7a9f0dec7f6f0160edc39329fe927
Maia Kapur <39382798+mkapur-noaa@users.noreply.github.com>
2024-04-30 14:00:53 -0700

    rough in three steps for master
    paralleli

--- R/master.r
+++ R/master.r
@@ -1,5 +1,7 @@
 ## evOsmose-Master
 ## execute the full study in parallel
+## M S Kapur Feb 2024 ++
+## maia.kapur@noaa.gov
 
 rm(list = ls()) ## clear workspace
 invisible(lapply(list.files(here::here('R','functions'), full.names = TRUE), FUN=source)) ## load all functions and presets
@@ -8,30 +10,32 @@ cores <- detectCores() - 2
 cl <- makeCluster(cores)
 registerDoParallel(cl)
 
-#1,7,9,13
-
-build_Data(scenario = 2,
-           sppIdx = 7,
-           replicate = 1,
-           yrs_use = 2010:2044)
+## Create input data ----
+## Only need to do this if the simulations themselves or sampling protocol have changed
+## (e.g., obs error, survey array, years or frequency thereof, etc)
 
 ## for one species-replicate combo, four scenarios takes about 20 seconds
 foreach(scenario=1:4) %dopar% {
   invisible(lapply(list.files(here::here('R','functions'), full.names = TRUE), FUN=source)) ## load all functions and presets
 
-  for(species in c(1,7,9,13)) {
+  for(species in (sppLabs2$Var3[sppLabs2$Var4]+1)) {
     build_Data(scenario,
                sppIdx = species,
-               repuse = 1,
-               yrs_use = 2010:2044,
-               obs_error = 0.2, ## whether or not survey biomass has observation error
-               units = 'numbers',
-               units_scalar = 1e6)
-    #
-    #sim <- replicate(1,  {retry_function(function()run_MSE(nprojyrs, neqyrs,spaceID), 1000)})
+               repID = 1, ## replicates 1:29
+               yrs_use = 2010:2080, ## years to extract data for
+               srv_selex = 11, ## age at 50% selex
+               obs_error = 0.2, ## observation error for surveys
+               units = 'biomass',
+               units_scalar = 1,
+               do_GAM = FALSE)
   } ## end species loop
 } ## end scenario loop (parallel)
 
 # When you're done, clean up the cluster
 stopImplicitCluster()
 # stopCluster()
+
+## Run WHAM models ----
+
+
+## Summarize results across all species, scenarios, simulations ----
