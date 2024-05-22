d7e734b536b4b5e39a86596f51ae232490ecc21a
Maia Kapur <39382798+mkapur-noaa@users.noreply.github.com>
2024-05-22 10:38:45 -0700

    turn off fracyr_spawn setting

--- R/functions/run_WHAM.r
+++ R/functions/run_WHAM.r
@@ -65,7 +65,7 @@ run_WHAM <-function(yrs_use = 2010:2080, ## years to run the assessment
   #
   # #* initial pars, phase, lambdas ----
   asap3$dat$N1_ini <- as.matrix(N_init, nrow = 1)
-  asap3$dat$fracyr_spawn <- ifelse(spname=='AtlanticHerring',0.05,0.5)
+  # asap3$dat$fracyr_spawn <- ifelse(spname=='AtlanticHerring',0.05,0.5)
   # # asap3$dat$lambda_steepness
   # # asap3$dat$phase_steepness <- 2
   # # asap3$dat$q_ini
