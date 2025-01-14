27e7c9b03485b32d55ec380458eed0dc8aab1f2e
Maia Kapur <39382798+mkapur-noaa@users.noreply.github.com>
2025-01-14 14:39:46 -0800

    document stats for writeup

--- R/manuscript_stuff.R
+++ R/manuscript_stuff.R
@@ -5,8 +5,14 @@ invisible(lapply(list.files(
 ), FUN = source)) ## load all functions and presets
 
 ## RESULTS ----
+#* Data Generation ----
+full_df %>%
+  filter(!is.na(abund_cv) & fc %in% c(1,0.15)) %>%
+  summarise(mean(abund_cv), .by = c('fc','species')) %>%
+  arrange(fc, species)
+
 #* Assessment ----
-## convergence/execution rates, uses mre_all0 and mre_all from master.R
+#** diagnostics ----
 total_runs <- length(unique(mre_all0$replicate)) *
   length(unique(mre_all0$species)) *
   length(unique(mre_all0$scenario)) *
@@ -21,32 +27,56 @@ length(unique(mre_all_filter$replicate)) *
   length(unique(mre_all_filter$scenario)) *
   length(unique(mre_all_filter$fc))
 
-## diagnostics
-all_mods_use  <- list.files(
-  files_to_run,
-  pattern = 'model.rdata',
-  recursive = TRUE,
-  full.names = TRUE ) %>%
-  lapply(., FUN = load, .GlobalEnv) %>%
-  bind_rows() %>%
-  dplyr::select(-inputN) %>%
-  filter(q_treatment == 'fixed' & ewaa == 'perfect' & fc != 0.15001 ) %>%
-  distinct() ## drop old runs and duplicates
+## diagnostic pass rates
+diags %>%
+  filter(!is.na(runs_test_passed)) %>%
+  summarise(sum(runs_test_passed)/n(),.by = c('fc'))
 
-inpt <- mod_use$input
-rpt <- mod_use$report()
 
-pred_obs_surv <- inpt$data$obs %>%
-  filter(type == 'logindex') %>%
-  mutate(year = year+2009,
-         yt = val) %>%
-  merge(.,data.frame(yhat = rpt$pred_log_indices) %>%
-           mutate(year = 2010:2080) %>%
-           filter(year %% 2 == 0), by = 'year') %>%
-  dplyr::select(year, yt, yhat) %>%
-  mutate(resid = (yhat - yt)^2)
+tt0 <- full_df0 %>%
+  filter(!(fc %in% c(0.5,0.05))) %>%
+  summarise(n=length(unique(replicate)),.by = c('fc','species','scenario'))
+sum(tt0$n)
+tt <- full_df %>%
+  filter(!(fc %in% c(0.5,0.05))) %>%
+  summarise(n=length(unique(replicate)),.by = c('fc','species','scenario'))
+sum(tt$n)
 
-sqrt(sum(pred_obs_surv$resid)/nrow(pred_obs_surv))
+
+#** performance metrics ----
+
+## MARE by species x fc
+full_df %>%
+  filter(!(fc %in% c(0.5,0.05)) & !is.na(MRE_scaled) & year < 2060) %>%
+  summarise(median(MRE_scaled),.by = c('fc','species')) %>%
+  arrange(fc, species)
+
+
+# all_mods_use  <- list.files(
+#   files_to_run,
+#   pattern = 'model.rdata',
+#   recursive = TRUE,
+#   full.names = TRUE ) %>%
+#   lapply(., FUN = load, .GlobalEnv) %>%
+#   bind_rows() %>%
+#   dplyr::select(-inputN) %>%
+#   filter(q_treatment == 'fixed' & ewaa == 'perfect' & fc != 0.15001 ) %>%
+#   distinct() ## drop old runs and duplicates
+#
+# inpt <- mod_use$input
+# rpt <- mod_use$report()
+#
+# pred_obs_surv <- inpt$data$obs %>%
+#   filter(type == 'logindex') %>%
+#   mutate(year = year+2009,
+#          yt = val) %>%
+#   merge(.,data.frame(yhat = rpt$pred_log_indices) %>%
+#            mutate(year = 2010:2080) %>%
+#            filter(year %% 2 == 0), by = 'year') %>%
+#   dplyr::select(year, yt, yhat) %>%
+#   mutate(resid = (yhat - yt)^2)
+#
+# sqrt(sum(pred_obs_surv$resid)/nrow(pred_obs_surv))
 
 
 
@@ -94,21 +124,21 @@ surv1 <- baseplot +
 surv15 <- baseplot +
   geom_point(data = survey_15, aes(x = lat, y = long), fill = 'blue', color = 'blue')+
   annotate(
-    "text", label = "B)",
+    "text", label = "C)",
     x = 2, y = 50, size = 6
   )
 
 surv05 <- baseplot +
   geom_point(data = survey_05, aes(x = lat, y = long), fill = 'blue', color = 'blue')+
   annotate(
-    "text", label = "C)",
+    "text", label = "D)",
     x = 2, y = 50, size = 6
   )
 
 surv50 <- baseplot +
   geom_point(data = survey_50, aes(x = lat, y = long), fill = 'blue', color = 'blue') +
   annotate(
-    "text", label = "D)",
+    "text", label = "B)",
     x = 2, y = 50, size = 6
   )
 
@@ -119,4 +149,25 @@ ggsave(last_plot(),
        file = here('figs','manuscript','supplement_1.png'),
        width = 8, height = 4, unit = 'in', dpi = 520)
 
-# assign(value = tmp, x =paste0('sim',isim))
+
+## uncertainty in add'l runs
+full_df %>%
+  filter(!is.na(abund_cv) & fc %in% c(0.5,0.05)) %>%
+  summarise(mean(abund_cv), .by = c('fc','species')) %>%
+  arrange(fc, species)
+## replicate 8 failed for herring no-no 0.05; drop everywhere for sens
+
+## original dims
+tt0 <- full_df0 %>%
+  filter(fc %in% c(0.5,0.05)) %>%
+  summarise(n=length(unique(replicate)),.by = c('fc','species','scenario'))
+sum(tt0$n)
+
+## filter diags and drop rep 8
+tt <- full_df %>%
+  filter(fc %in% c(0.5,0.05) & replicate != 8 ) %>%
+  summarise(n=length(unique(replicate)),.by = c('fc','species','scenario'))
+sum(tt$n) ## final total
+
+
+
